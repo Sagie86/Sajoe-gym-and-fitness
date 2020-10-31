c31329176cdd3b7db40de68c9628b6db04908784
Sagie86 <sagie.ighodaro@yahoo.co.uk>
2020-10-31 18:58:46 +0000

    second part of deployment process

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn sajoe_gym.wsgi:application
--- sajoe_gym/settings.py
+++ sajoe_gym/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'iu_)n9%t@ivz990luyn^#-4dkq$508)#v%03!jwp1-+=x-)xo2'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['sajoe-gym-and-fitness.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -119,12 +119,17 @@ WSGI_APPLICATION = 'sajoe_gym.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
-   'default': {
-       'ENGINE': 'django.db.backends.sqlite3',
-       'NAME': BASE_DIR / 'db.sqlite3',
-   }
-}
+if 'DATABASE_URL' in os.environ:
+    DATABASE = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+    }
+else:
+    DATABASES = {
+      'default': {
+          'ENGINE': 'django.db.backends.sqlite3',
+          'NAME': BASE_DIR / 'db.sqlite3',
+       }
+    }
 
 
 # Password validation
