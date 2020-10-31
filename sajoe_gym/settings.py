f0205a5cdc6933d354fdfdf957c27ead3ab72d5b
Sagie86 <sagie.ighodaro@yahoo.co.uk>
2020-10-31 19:31:35 +0000

    removed secret key and set debug

--- sajoe_gym/settings.py
+++ sajoe_gym/settings.py
@@ -22,10 +22,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'iu_)n9%t@ivz990luyn^#-4dkq$508)#v%03!jwp1-+=x-)xo2'
-
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['sajoe-gym-and-fitness.herokuapp.com', 'localhost']
 
